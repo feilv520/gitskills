daa252485d5e911677ceb627f841e2a15f2d5d37
apple <apple@applematoiMac.local>
2015-07-03 11:04:58 +0800

    first commit xcodeproj

---
+++ 123/123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by apple on 15/6/24.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
